345a0b731bb9d474a69541dff5ba700ef70139d8
ArunPaulGopal <ArunPaulGopal@Gmail.com>
2016-03-30 16:01:05 -0700

    Issue #4: Bootstrap modified, function added to build User Blogs, and user can navigate between Timeline and Blog page.

--- default.js
+++ default.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
     $('#enter').click(function(){
         $('#timeline').toggleClass("hide");
         $('#welcome').toggleClass("hide");
@@ -45,20 +46,59 @@ $(document).ready(function() {
             $('#blogresults').append(panel);
         }
     }
-});
 
-$('#blogsbutton').click(function(){
-    $('#timeline').toggleClass("hide");
-    $('#timelinebutton').toggleClass("hide");
+    $('#blogsbutton').click(function(){
+        $('#blogsbutton').toggleClass("hide");
+        $('#timeline').toggleClass("hide");
+        $('#timelinebutton').toggleClass("hide");
+        $('#yourblogs').toggleClass("hide");
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET','/yourblogs');
+        xhr.send(null);
+        xhr.addEventListener('load',function(){
+            var myData = JSON.parse(xhr.responseText);
+            myBlogs(myData);
+        })
+    });
 
-    $('#yourblogs').toggleClass("hide");
+    var myBlogs = function(array){
+        for (i=0; i<array.length; i++){
+            var title = array[i].blogtitle;
+            var content = array[i].blogcontent;
+            var imagesrc = array[i].blogimage;
+            var media = document.createElement('div');
+            var medialeft = document.createElement('div');
+            var mediabody = document.createElement('div');
+            var panel = document.createElement('div');
+            var panelhead = document.createElement('div');
+            var panelbody = document.createElement('div');
+            var panelfooter = document.createElement('div');
+            var image = document.createElement('img');
+            image.setAttribute('src',imagesrc);
+            $(image).addClass('img-rounded thumbnail');
+            $(media).addClass('media');
+            $(mediabody).addClass('media-body');
+            $(medialeft).addClass('media-left');
+            $(panel).addClass('panel panel-success');
+            $(panelhead).addClass('panel panel-heading');
+            $(panelbody).addClass('panel panel-body');
+            $(panelfooter).addClass('panel panel-footer');
+            $(medialeft).append(image);
+            $(panelhead).append(title);
+            $(mediabody).append(content);
+            $(media).append(medialeft);
+            $(media).append(mediabody);
+            $(panelbody).append(media);
+            $(panel).append(panelhead);
+            $(panel).append(panelbody);
+            $('#yourblogresults').append(panel);
+        }
+    }
 
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET','/yourblogs');
-    xhr.send(null);
-    xhr.addEventListener('load',function(){
-        var myData = JSON.parse(xhr.responseText);
-
-    })
-    $('#blogsbutton').toggleClass("hide");
+    $('#timelinebutton').click(function(){
+        $('#blogsbutton').toggleClass("hide");
+        $('#timeline').toggleClass("hide");
+        $('#timelinebutton').toggleClass("hide");
+        $('#yourblogs').toggleClass("hide");
+    });
 });
